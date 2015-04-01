a693498d9022b6e53d4db7c3a055d787e8bdc85b
Alexey Brovikov <alexey.brovikov@gmail.com>
2015-04-02 02:10:24 +0700

    S3 secure credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAIOQNVCQ2W6T4PBAA',                        # required
-    :aws_secret_access_key  => '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', 
+    :aws_access_key_id      => ENV['S3_KEY'],                        # required
+    :aws_secret_access_key  => ENV['S3_SECRET'], 
     :region                 => 'us-west-2'                  # optional, defaults to 'us-east-1'
     #:host                   => 'https://s3-us-west-2.amazonaws.com/test-book',             # optional, defaults to nil
     # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
